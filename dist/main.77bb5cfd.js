0ecbcaabf82e5d501067dbe13748f7bc48524d8d
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2022-12-16 23:26:33 -0800

    Small edits to the mobile view

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54038" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65471" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
